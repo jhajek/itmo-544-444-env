ecbc96734592fbd000e3801abedeb574ab8e84a7
Jeremy Hajek <hajek@iit.edu>
2015-09-24 10:58:21 -0500

    Added code to pull new index.html from other githuib account

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git 
+
+git clone https://github.com/jhajek/itmo-544-444-fall2015.git
+
+mv ./itmo-544-444-fall2015/images /var/www/html/images
+mv ./itmo-544-444-fall2015/index.html /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
