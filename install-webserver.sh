4f7143af2ce5bb41ea4bc236005d15dd4915ff75
Jeremy Hajek <hajek@iit.edu>
2015-10-10 14:17:09 -0500

    add php sdk

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git 
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl 
 
 git clone https://github.com/jhajek/itmo-544-444-fall2015.git
 
 mv ./itmo-544-444-fall2015/images /var/www/html/images
 mv ./itmo-544-444-fall2015/index.html /var/www/html
+
+curl -sS https://getcomposer.org/installer | php
+
+php composer.phar require aws/aws-sdk-php
+
+mv vendor /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
