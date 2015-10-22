e18a32b0252074a1a1b38f9a37484925f643d909
Jeremy Hajek <hajek@iit.edu>
2015-10-22 15:01:14 -0500

    Added debugging

--- install-webserver.sh
+++ install-webserver.sh
@@ -9,13 +9,11 @@ mv ./itmo-544-444-fall2015/images /var/www/html/images
 mv ./itmo-544-444-fall2015/index.html /var/www/html
 mv ./itmo-544-444-fall2015/*.php /var/www/html
 
-curl -sS https://getcomposer.org/installer | php
+curl -sS https://getcomposer.org/installer | php &> /tmp/getcomposer.txt
 
-php composer.phar require aws/aws-sdk-php
+php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
-mv vendor /var/www/html
+mv vendor /var/www/html &> /tmp/movevendor.txt
 
 echo "Hello!" > /tmp/hello.txt
 
-PW=letmein
-export PW 
